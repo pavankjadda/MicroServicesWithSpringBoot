0964b41ce159a82125a0d08f03265a45ad9d25ed
Pavan Jadda <jpavanaryan@gmail.com>
2019-02-04 15:41:12 -0500

    Updated Config

--- gateway/src/main/java/com/microservices/GatewayApplication.java
+++ gateway/src/main/java/com/microservices/GatewayApplication.java
@@ -5,16 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
 @EnableZuulProxy
+@SpringBootApplication
 public class GatewayApplication
 {
-
 	public static void main(String[] args)
 	{
 		SpringApplication.run(GatewayApplication.class, args);
 	}
-	
 	@Bean
 	public PreFilter preFilter()
 	{
